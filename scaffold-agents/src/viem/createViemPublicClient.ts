32975f6e5219f1a6bc49bacfd5e82bdde45ac5ae
sooravally <sooravally25@gmail.com>
2025-06-05 07:21:16 +0530

    commit

--- scaffold-agents/src/viem/createViemPublicClient.ts
+++
@@ -1,9 +0,0 @@
-import { createPublicClient, http } from 'viem'
-import { moonbaseAlpha } from 'viem/chains'
-
-export function createViemPublicClient() {
-    return createPublicClient({
-        chain: moonbaseAlpha,
-        transport: http(),
-    });
-}
